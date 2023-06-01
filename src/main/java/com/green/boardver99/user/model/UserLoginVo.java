b19e72736c9cb062fa22e67d07c09fd96f37fa7c
성수천 <3598ysm@gmail.com>
2023-06-01 18:03:52 +0900

    기본설정

--- src/main/java/com/green/boardver99/user/model/UserLoginVo.java
+++ src/main/java/com/green/boardver99/user/model/UserLoginVo.java
@@ -6,5 +6,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class UserLoginVo {
-
+    private int iuser;
+    private String uid;
+    private String upw;
+    private String nm;
+    private char gender;
+    private String addr;
+    private String main_pic;
+    private String created_At;
+    private String updated_At;
 }
