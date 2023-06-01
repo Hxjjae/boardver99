9ffe1f3b93df9bf7185f4da2f722b8d005da9d7f
진혁구 <jhj02201@gmail.com>
2023-06-01 17:47:11 +0900

    first

--- src/main/java/com/green/boardver99/user/UserService.java
+++ src/main/java/com/green/boardver99/user/UserService.java
@@ -1,4 +1,19 @@
 package com.green.boardver99.user;
 
+import com.green.boardver99.user.model.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private final UserMapper mapper;
+
+    @Autowired
+    public UserService(UserMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public int insUser(UserEntity entity) {
+        return mapper.insUser(entity);
+    }
 }
