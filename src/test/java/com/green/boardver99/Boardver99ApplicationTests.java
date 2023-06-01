5daffcda78e30444496efc346353ad1eb5b0d2d8
진혁구 <jhj02201@gmail.com>
2023-06-01 09:29:00 +0900

    first

---
+++ src/test/java/com/green/boardver99/Boardver99ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.boardver99;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boardver99ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
