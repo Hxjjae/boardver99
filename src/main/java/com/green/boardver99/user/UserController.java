3b3f7c56a00b8ffbedaa98c4ae462f309d00c221
배성현 <zkti12345@naver.com>
2023-06-01 17:49:35 +0900

    ssss

--- src/main/java/com/green/boardver99/user/UserController.java
+++ src/main/java/com/green/boardver99/user/UserController.java
@@ -1,4 +1,30 @@
 package com.green.boardver99.user;
 
+import com.green.boardver99.user.model.UserEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    private final UserService service;
+
+    @Autowired
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    @Operation(summary = "회원가입", description = "" +
+            "uid: [20] 회원 아이디, <br>" +
+            "upw : [100] 회원 비밀번호,<br>" +
+            "nm : [30] 회원명,<br>" +
+            "gender: [1] 성별(M: 남성, F:여성),<br>"+
+            "addr: [100] 대구시 달서구")
+   public int postUser(@RequestBody UserEntity entity){
+        return service.
+    }
+
 }
